da3737af439571990e21d3cdacaac6fbbd297761
SANTOSH KUMAR MISHRA <34652426+santoshmishradd@users.noreply.github.com>
2019-06-28 20:28:13 +0530

    Add files via upload

---
+++ 000.spyder_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 12:30:12 2018
+
+@author: Santosh Mishra
+"""
+
+def main():
+    print("Hello World")
+    
+if __name__== "__main__":
+    main()
+    
